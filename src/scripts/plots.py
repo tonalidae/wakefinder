97b4e0feddadfcaa348ac36a4c665c557b377fe6
Stephanie Cely <eccelyr@unal.edu.co>
2023-06-22 15:02:10 -0500

    fix(3dplot): marker size up

--- src/scripts/plots.py
+++ src/scripts/plots.py
@@ -1214,7 +1214,7 @@ def plot_3d_plt(arr1, lmc, title):
     ax = fig.add_subplot(111, projection='3d')
 
     # 3D scatter plot for arr1
-    scatter = ax.scatter(arr1[:, 0], arr1[:, 1], arr1[:, 2], c=arr1[:, 10], cmap='viridis', s=1.5, alpha=0.7, label='wake', edgecolors='none')
+    scatter = ax.scatter(arr1[:, 0], arr1[:, 1], arr1[:, 2], c=arr1[:, 10], cmap='viridis', s=2, alpha=0.8, label='wake', edgecolors='none')
 
     # 3D line plot for arr2
     ax.plot(lmc[:, 0], lmc[:, 1], lmc[:, 2], color='orange', alpha=0.8, linewidth=1)
