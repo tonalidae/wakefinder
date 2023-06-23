274c3dedf68aba50059340b9e5ba534b00018242
Stephanie Cely <eccelyr@unal.edu.co>
2023-06-23 06:17:04 -0500

    test(pl): energy cont rm 1 halo

--- src/scripts/plots.py
+++ src/scripts/plots.py
@@ -2491,54 +2491,43 @@ def E_L_contour(halo, proj, halo2=None, slice=None):
     if proj == "x":
         x_data = halo[:, 11]
         y_data = halo[:, 15]
-        x2_data = halo2[:, 11]
-        y2_data = halo2[:, 15]
         
-        # Create 2D histograms and filled contour plots for each halo
-        hist1, xedges1, yedges1 = np.histogram2d(x_data, y_data, bins=50)
-        X1, Y1 = np.meshgrid(xedges1[:-1], yedges1[:-1])
-        cf1 = ax.contourf(X1, Y1, hist1.T, cmap='Blues', alpha=0.7)
-
-        hist2, xedges2, yedges2 = np.histogram2d(x2_data, y2_data, bins=50)
-        X2, Y2 = np.meshgrid(xedges2[:-1], yedges2[:-1])
-        cf2 = ax.contourf(X2, Y2, hist2.T, cmap='Oranges', alpha=0.7)
-
-
-        ax.set_xlabel(r'Momento angular $L_x$ (kpc km/s)', fontsize=14)
-        ax.set_ylabel(r"Energia ($\frac{\mathrm{km}^2}{\mathrm{s}^2}$)", fontsize=14)
+        ax.set_xlabel(r'Specific angular momentum $L_x$ (kpc km/s)', fontsize=14)
         ax.set_title(r'Phase diagram $E$ vs $L_x$', fontsize=20)
-    elif proj == "z":
-        x_data = halo[:, 13]
+    elif proj == "y":
+        x_data = halo[:, 12]
         y_data = halo[:, 15]
-        x2_data = halo2[:, 13]
-        y2_data = halo2[:, 15]
+
+        ax.set_xlabel(r'Specific angular momentum $L_y$ (kpc km/s)', fontsize=14)
+        ax.set_title(r'Phase diagram $E$ vs $L_y$', fontsize=20)
+    elif proj == "z":
+        x_data = halo2[:, 13]
+        y_data = halo2[:, 15]
         
-        # Create 2D histogram and contour plot
-        hist, xedges, yedges = np.histogram2d(np.concatenate((x_data, x2_data)), np.concatenate((y_data, y2_data)), bins=50)
-        X, Y = np.meshgrid(xedges[:-1], yedges[:-1])
-        ax.contour(X, Y, hist.T, cmap='viridis')
+        ax.set_xlabel(r'Specific angular momentum $L_z$ (kpc km/s)', fontsize=14)
+        ax.set_title(r'Phase diagram $E$ vs $L_z$', fontsize=20)
     elif proj == "mag":
         x_data = halo[:, 10]
         y_data = halo[:, 15]
-        x2_data = halo2[:, 10]
-        y2_data = halo2[:, 15]
         
-        # Create 2D histogram and contour plot
-        hist, xedges, yedges = np.histogram2d(np.concatenate((x_data, x2_data)), np.concatenate((y_data, y2_data)), bins=50)
-        X, Y = np.meshgrid(xedges[:-1], yedges[:-1])
-        ax.contour(X, Y, hist.T, cmap='viridis')
+        ax.set_xlabel(r'Magnitude of specific angular momentum $L$ (kpc km/s)', fontsize=14)
+        ax.set_title(r'Phase diagram $E$ vs $L$', fontsize=20)
+    else:
+        raise ValueError("Invalid projection")
+        
+    # Create 2D histograms and filled contour plots for each halo
+    
+    hist1, xedges1, yedges1 = np.histogram2d(x_data, y_data, bins=50)
+    X1, Y1 = np.meshgrid(xedges1[:-1], yedges1[:-1])
+    cf1 = ax.contourf(X1, Y1, hist1.T, cmap='Blues', alpha=0.7)
 
-        ax.set_xlabel(r'Momento angular $L_x$ (kpc km/s)', fontsize=14)
-        ax.set_ylabel(r"Energia ($\frac{\mathrm{km}^2}{\mathrm{s}^2}$)", fontsize=14)
-        ax.set_title(r'Phase diagram $E$ vs $L_x$', fontsize=20)
-        ax.set_aspect('equal', adjustable='box')
-        plt.show()
     # Add colorbars for each contour plot
     cbar1 = fig.colorbar(cf1, ax=ax, shrink=0.5, aspect=10, pad=0.02)
     cbar1.set_label('Unperturbed Halo Density', fontsize=12)
-    cbar2 = fig.colorbar(cf2, ax=ax, shrink=0.5, aspect=10, pad=0.1)
-    cbar2.set_label('Perturbed Halo Density', fontsize=12)
+    # cbar2 = fig.colorbar(cf2, ax=ax, shrink=0.5, aspect=10, pad=0.1)
+    # cbar2.set_label('Perturbed Halo Density', fontsize=12)
     
+    ax.set_ylabel(r"Energy ($\frac{\mathrm{km}^2}{\mathrm{s}^2}$)", fontsize=14)
     ax.set_aspect('equal', adjustable='box')
 
     plt.show()
